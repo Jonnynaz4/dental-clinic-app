70bfb89921e757b00e08e520bb200dbebdb3a847
Jonnynaz4 <68715917+Jonnynaz4@users.noreply.github.com>
2025-10-25 17:18:31 -0600

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ if (!MONGODB_URI) {
 // Middleware
 app.use(cors({
     origin: [
-        'https://whimsical-phoenix-1e9b16.netlify.app',
+        'https://cozy-smakager-e1db95.netlify.app/',
         'https://dental-clinic-app-production.up.railway.app',
         'http://localhost:3000'
     ],
